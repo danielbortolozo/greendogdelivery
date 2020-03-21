cc7d1b601d71fd907a49a0d1018e02674b678316
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2020-03-21 15:00:05 -0300

    first commit

---
+++ src/main/java/br/com/sisdb/greendogdelivery/greendogdelivery/model/Cliente.java
@@ -0,0 +1,135 @@
+package br.com.sisdb.greendogdelivery.greendogdelivery.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+public class Cliente {
+	
+	
+	@Id
+	@GeneratedValue
+		(strategy=GenerationType.IDENTITY)
+	private	Long id;
+	
+	@NotNull
+	@Length(min=2,	max=30,message="O tamanho do nome deve ser entre {min} e {max} caracteres")
+	private	String nome;
+	@NotNull
+	@Length(min=2,	max=300,message="O tamanho do endereço deve ser entre {min} e {max} caracteres")
+	private	String endereco;
+	@OneToMany(mappedBy	= "cliente",fetch = FetchType.EAGER)
+	@Cascade(CascadeType.ALL)
+	private	List<Pedido> pedidos;
+	
+	
+		
+	public Cliente() {
+		super();
+	}
+	public Cliente(Long id, String nome, String endereco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.endereco = endereco;	
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
+public void novoPedido(Pedido pedido) {
+		
+		if (this.pedidos==null) pedidos = new ArrayList<Pedido>();
+		
+		pedidos.add(pedido);		
+}
+@Override
+public String toString() {
+	return "Cliente [id=" + id + ", nome=" + nome + ", endereco=" + endereco + ", pedidos=" + pedidos + "]";
+}
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+	result = prime * result + ((pedidos == null) ? 0 : pedidos.hashCode());
+	return result;
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Cliente other = (Cliente) obj;
+	if (endereco == null) {
+		if (other.endereco != null)
+			return false;
+	} else if (!endereco.equals(other.endereco))
+		return false;
+	if (id == null) {
+		if (other.id != null)
+			return false;
+	} else if (!id.equals(other.id))
+		return false;
+	if (nome == null) {
+		if (other.nome != null)
+			return false;
+	} else if (!nome.equals(other.nome))
+		return false;
+	if (pedidos == null) {
+		if (other.pedidos != null)
+			return false;
+	} else if (!pedidos.equals(other.pedidos))
+		return false;
+	return true;
+}
+
+
+
+
+
+	
+	
+	
+	
+	
+}
